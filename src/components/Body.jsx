c69ee13d20949ec0e8d75f3b6c87056a53353372
JohnsonOdang <johnnyodanny@gmail.com>
2024-03-26 05:24:50 -0700

    React_Spotify-clone

---
+++ src/components/Body.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useStateProvider } from "../utils/StateProvider";
+import { AiFillClockCircle } from "react-icons/ai";
+import { reducerCases } from "../utils/Constants";
+
+export default function Body({ headerBackground}) {
+  const [{ token, selectedPlaylist, selectedPlaylistId }, dispatch] =
+    useStateProvider();
+  
+  useEffect(() => {
+    const getInitialPlaylist = async () => {
+      const response = await axios.get(
+        `https://api.spotify.com/v1/playlists/${selectedPlaylistId}`,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const selectedPlaylist = {
+        id: response.data.id,
+        name: response.data.name,
+        description: response.data.description.startsWith("<a")
+          ? ""
+          : response.data.description,
+        image: response.data.images[0].url,
+        tracks: response.data.tracks.items.map(({ track }) => ({
+          id: track.id,
+          name: track.name,
+          artists: track.artists.map((artist) => artist.name),
+          image: track.album.images[2].url,
+          duration: track.duration_ms,
+          album: track.album.name,
+          context_uri: track.album.uri,
+          track_number: track.track_number,
+        })),
+      };
+      dispatch({ type: reducerCases.SET_PLAYLIST, selectedPlaylist });
+    };
+    getInitialPlaylist();
+  }, [token, dispatch, selectedPlaylistId]);
+  return (
+    <div>Body</div>
+  )
+}
+
+ 
